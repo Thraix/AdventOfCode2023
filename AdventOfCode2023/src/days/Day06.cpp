70e289f09c006380fa4aeeb656a6c8860f6092d2
Thraix <thraix@hotmail.com>
2023-12-06 23:02:21 +0100

    Add Day6 solution

--- AdventOfCode2023/src/days/Day06.cpp
+++ AdventOfCode2023/src/days/Day06.cpp
@@ -2,31 +2,68 @@
 
 namespace day06
 {
-  REGISTER_DAY(day06, std::vector<int>, int);
+  using Game = std::pair<int, int>;
+  REGISTER_DAY(day06, std::vector<Game>, int);
 
-  REGISTER_TEST_EXAMPLE(day06, ExampleInput, 1, 0);
-  REGISTER_TEST        (day06, Input,        1, 0);
-  REGISTER_TEST_EXAMPLE(day06, ExampleInput, 2, 0);
-  REGISTER_TEST        (day06, Input,        2, 0);
+  REGISTER_TEST_EXAMPLE(day06, ExampleInput, 1, 288);
+  REGISTER_TEST        (day06, Input,        1, 4811940);
+  REGISTER_TEST_EXAMPLE(day06, ExampleInput, 2, 71503);
+  REGISTER_TEST        (day06, Input,        2, 30077773);
 
   READ_INPUT(input)
   {
     std::string str;
-    std::vector<int> ints;
-    while (std::getline(input, str))
+    std::vector<std::pair<int, int>> games;
+    std::getline(input, str);
+    std::stringstream ss{str};
+    int i;
+    ss >> "Time: ";
+    while (ss >> i) games.emplace_back(i, 0);
+
+    std::getline(input, str);
+    std::stringstream ss2{str};
+    ss2 >> "Distance: ";
+
+    int count = 0;
+    while (ss2 >> i)
     {
-        ints.emplace_back(std::stoi(str));
+      games[count].second = i;
+      count++;
     }
-    return ints;
+    return games;
   }
 
   OUTPUT1(input)
   {
-    return 0;
+    int product = 1;
+    for(auto& [time, distance] : input)
+    {
+      int lowerBound = std::floor(time / 2.0 - sqrt(time * time / 4.0 - distance)) + 1;
+      int upperBound = std::ceil(time / 2.0 + sqrt(time * time / 4.0 - distance)) - 1;
+
+      product *= upperBound - lowerBound + 1;
+    }
+    return product;
   }
 
   OUTPUT2(input)
   {
-    return 0;
+    int64_t time = 0;
+    int64_t distance = 0;
+    std::string sTime = "";
+    std::string sDistance = "";
+    for (auto& game : input)
+    {
+      sTime += std::to_string(game.first);
+      sDistance += std::to_string(game.second);
+    }
+    char* endPtr;
+    time = std::strtoll(sTime.c_str(), &endPtr, 10);
+    distance = std::strtoll(sDistance.c_str(), &endPtr, 10);
+
+    int lowerBound = std::floor(time / 2.0 - sqrt(time * time / 4.0 - distance)) + 1;
+    int upperBound = std::ceil(time / 2.0 + sqrt(time * time / 4.0 - distance)) - 1;
+
+    return upperBound - lowerBound + 1;
   }
 }
