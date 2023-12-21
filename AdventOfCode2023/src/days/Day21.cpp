0e4325d169e116f1c5e05589bf3b8122a97bd657
Thraix <thraix@hotmail.com>
2023-12-21 08:02:58 +0100

    Part1 done

--- AdventOfCode2023/src/days/Day21.cpp
+++ AdventOfCode2023/src/days/Day21.cpp
@@ -2,31 +2,63 @@
 
 namespace day21
 {
-  REGISTER_DAY(day21, std::vector<int>, int);
+  REGISTER_DAY(day21, Array2D<char>, int);
 
-  REGISTER_TEST_EXAMPLE(day21, ExampleInput, 1, 0);
-  REGISTER_TEST        (day21, Input,        1, 0);
+  // REGISTER_TEST_EXAMPLE(day21, ExampleInput, 1, 16);
+  REGISTER_TEST        (day21, Input,        1, 3651);
   REGISTER_TEST_EXAMPLE(day21, ExampleInput, 2, 0);
   REGISTER_TEST        (day21, Input,        2, 0);
 
   READ_INPUT(input)
   {
-    std::string str;
-    std::vector<int> ints;
-    while (std::getline(input, str))
-    {
-        ints.emplace_back(std::stoi(str));
-    }
-    return ints;
+    return Input::ReadArray2D(input);
   }
 
+  // 1282 too low
   OUTPUT1(input)
   {
-    return 0;
+    Index2D pos = input.Find('S');
+
+    std::set<Index2D> exactly;
+    std::set<Index2D> visited;
+    std::queue<std::pair<Index2D, int>> stack{};
+    stack.push(std::make_pair(pos, 0));
+
+    while (!stack.empty())
+    {
+      std::pair<Index2D, int> pos = stack.front();
+      stack.pop();
+      if (pos.second % 2 == 0)
+      {
+        exactly.emplace(pos.first);
+        if(pos.second == 64)
+          continue;
+      }
+
+      for (auto& i : input.GetNeighbors(pos.first, false))
+      {
+        if (input.Get(i) != '#' && visited.emplace(i).second)
+        {
+          stack.push(std::make_pair(i, pos.second + 1));
+        }
+      }
+    }
+
+    auto cpy = input;
+    for(auto& i : exactly)
+    {
+      cpy.Set(i.x, i.y, 'O');
+    }
+    std::cout << cpy << std::endl;
+
+    return exactly.size();
   }
 
   OUTPUT2(input)
   {
+    int steps = 26501365;
+
+    int gridsFilled = steps / 0;
     return 0;
   }
 }
