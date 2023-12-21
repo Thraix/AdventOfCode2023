b44b8875afef7204fbe20db4ebf6b920855e9b8a
Thraix <thraix@hotmail.com>
2023-12-21 08:06:49 +0100

    More code

--- AdventOfCode2023/src/days/Day21.cpp
+++ AdventOfCode2023/src/days/Day21.cpp
@@ -35,11 +35,56 @@ namespace day21
           continue;
       }
 
-      for (auto& i : input.GetNeighbors(pos.first, false))
+      std::vector<Index2D> directions{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+      for (auto& i : directions)
       {
-        if (input.Get(i) != '#' && visited.emplace(i).second)
+        if (input.GetMod(pos.first.x + i.x, pos.first.y + i.y) != '#' && visited.emplace(pos.first + i).second)
         {
-          stack.push(std::make_pair(i, pos.second + 1));
+          stack.push(std::make_pair(pos.first + i, pos.second + 1));
+        }
+      }
+    }
+
+    auto cpy = input;
+    for(auto& i : exactly)
+    {
+      cpy.SetMod(i.x, i.y, 'O');
+    }
+    std::cout << cpy << std::endl;
+
+    return exactly.size();
+  }
+
+  OUTPUT2(input)
+  {
+#if 0
+    Index2D pos = input.Find('S');
+
+    std::set<Index2D> exactly;
+    std::set<Index2D> visited;
+    std::queue<std::pair<Index2D, int>> stack{};
+    stack.push(std::make_pair(pos, 0));
+
+    while (!stack.empty())
+    {
+      std::pair<Index2D, int> pos = stack.front();
+      stack.pop();
+      if (pos.second % 2 == 0)
+      {
+        exactly.emplace(pos.first);
+        if(pos.second == 5000) //26501365)
+          continue;
+      }
+
+      std::vector<Index2D> directions{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+      for (auto& i : directions)
+      {
+        Index2D newI{pos.first + i}:
+        if (input.GetMod(pos.first.x + i.x, pos.first.x + i.y) != '#' && visited.emplace(pos.first + i).second)
+        {
+          stack.push(std::make_pair(pos + i, pos.second + 1));
         }
       }
     }
@@ -52,13 +97,7 @@ namespace day21
     std::cout << cpy << std::endl;
 
     return exactly.size();
-  }
-
-  OUTPUT2(input)
-  {
-    int steps = 26501365;
-
-    int gridsFilled = steps / 0;
+#endif
     return 0;
   }
 }
